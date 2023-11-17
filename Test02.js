79593dda465705bdc93b45973c04d59c4bef8df3
John Doe <johndoe@example.com>
2023-11-17 23:38:13 +0700

    them giao diẹn moi

---
+++ Test02.js
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const Test02 = () => {
+  return (
+    <View>
+      <Text>Test02</Text>
+    </View>
+  );
+};
+
+export default Test02;
